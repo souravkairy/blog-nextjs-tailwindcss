ac10e41a67332267bf3b9c38e1284deb53a206f4
souravkairy <48748661+souravkairy@users.noreply.github.com>
2021-12-29 11:36:02 +0600

    footer updated

--- components/homePage/common-section/footer.js
+++ components/homePage/common-section/footer.js
@@ -25,7 +25,7 @@ const footer = ({ socialLinks }) => {
                     </a>
                 </div>
                 <div className="mt-8 md:mt-0 md:order-1">
-                    <p className="text-center text-base text-gray-400">&copy; 2020 Workflow, Inc. All rights reserved.</p>
+                    <p className="text-center text-base text-gray-400">COPYRIGHT &copy; SYED REZWANUL HAQUE {new Date().getFullYear()}.</p>
                 </div>
             </div>
         </footer >
