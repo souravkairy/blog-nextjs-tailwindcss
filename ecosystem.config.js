0f590bc9b12ef72c059f2393f76dc2b0dfc93830
souravkairy <48748661+souravkairy@users.noreply.github.com>
2022-01-01 17:44:03 +0600

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     apps: [
       {
         name: 'rezahaque.me',
-        cwd: ' /var/www/rezahaque.me',
+        cwd: ' /var/www/rezahaque.me/next/.next',
         script: 'npm',
         args: 'start',
         env: {},
